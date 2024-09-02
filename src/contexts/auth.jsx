8673600d959b515917a1f34941edc529f46a917c
fmancebo <f.92xavier1@gmail.com>
2024-09-02 15:56:54 -0300

    ajuste no context para verificaçao do token com retorno pro login page

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import { api, createSession } from "../services/api";
 
-// Criação do contexto
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
@@ -12,28 +11,44 @@ export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    const token = localStorage.getItem("token");
+    const checkToken = async () => {
+      const storedUser = localStorage.getItem("user");
+      const token = localStorage.getItem("token");
 
-    if (storedUser && token) {
-      setUser(JSON.parse(storedUser));
-      api.defaults.headers.Authorization = `Bearer ${token}`;
-    }
+      if (storedUser && token) {
+        api.defaults.headers.Authorization = `Bearer ${token}`;
 
-    setLoading(false);
-  }, []);
+        try {
+          const userId = JSON.parse(storedUser).id; // Certifique-se de que isso é válido
+          await api.get(`/user/${userId}/repositories`);
+          setUser(JSON.parse(storedUser));
+        } catch (error) {
+          // console.error("Token Check Error:", error);
+          localStorage.removeItem("user");
+          localStorage.removeItem("token");
+          navigate("/login");
+        }
+      }
+
+      setLoading(false);
+    };
+
+    checkToken();
+  }, [navigate]);
 
   const login = useCallback(
     async (email, password) => {
-      const response = await createSession(email, password); // Envie a senha
-
-      localStorage.setItem("user", JSON.stringify(response.data.user));
-      localStorage.setItem("token", response.data.token);
-
-      api.defaults.headers.Authorization = `Bearer ${response.data.token}`;
-
-      setUser(response.data.user);
-      navigate("/");
+      try {
+        const response = await createSession(email, password);
+        // console.log("Login Response:", response);
+        localStorage.setItem("user", JSON.stringify(response.data.user));
+        localStorage.setItem("token", response.data.token);
+        api.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+        setUser(response.data.user);
+        navigate("/");
+      } catch (error) {
+        // console.error("Login Error:", error);
+      }
     },
     [navigate],
   );
@@ -41,18 +56,14 @@ export function AuthProvider({ children }) {
   const logout = useCallback(() => {
     localStorage.removeItem("user");
     localStorage.removeItem("token");
-
     api.defaults.headers.Authorization = null;
-
     setUser(null);
-
     navigate("/login");
   }, [navigate]);
 
-  // Usando useMemo para memoizar o valor do contexto
   const value = useMemo(
     () => ({
-      authenticated: !!user, //  !! converte para boolean
+      authenticated: !!user,
       user,
       loading,
       login,
@@ -64,7 +75,6 @@ export function AuthProvider({ children }) {
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
-// Validação de props com PropTypes
 AuthProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
